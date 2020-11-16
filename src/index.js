e23dc216383d6a2e96d98a41bb3d97b9f1742eb0
frankie <26461046+frankiekie@users.noreply.github.com>
2020-11-16 00:39:39 -0500

    switches from async to generators
    moves "start" message to init func
    
    uses a generator to get the states and templates
    
    when a state is needed, it is generated on demand
    and then turned into an ArrayBuffer so it can be
    transferred instantly
    
    This will lead to a lot of cleaning up of unnecessarry code
    for tracking cache status and current status of fetch
    retrieval
    now the next step of the function won't happen
    until the first step is complete
    
    Also creates a beginning rewrite of rendering into its own
    method rather than into an event listener
    
    might be able to use getters and setters, not sure

--- src/index.js
+++ src/index.js
@@ -9,11 +9,8 @@ export default class DynamicCityDropdown {
 		this.worker = new Worker(
 			URL.createObjectURL(new Blob([`${this.defineWorker()}`])),
 		);
-		this.worker.postMessage({
-			type: 'START',
-			file: this.file,
-			fileType: this.type,
-		});
+		// this.worker.addEventListener('message', console.log);
+		// console.log(this.worker);
 	}
 
 	defineWorker() {
@@ -22,68 +19,162 @@ export default class DynamicCityDropdown {
 			states: {},
 			templates: {},
 		};
-
+		// Respone [ object, status, data ]
 		const script =
-			'const cache=' +
+			'let generator;const cache=' +
 			JSON.stringify(cache) +
 			';onmessage=' +
-			(({ data }) => {
-				if (data.type === 'START') {
-					cache.type = data.fileType;
-					return fetch(data.file)
-						.then(async resp => {
-							const output = {};
-							const body =
-								data.fileType == 'json'
-									? await resp.json()
-									: await resp.text();
-							if (data.fileType === 'json') {
-								for (let [state, cities] of Object.entries(
-									body,
-								)) {
-									if (output[state] === undefined)
-										output[state] = [];
-									let i = 0,
-										l = cities.length;
+			(async ({ data }) => {
 
-									for (; l > i++; )
-										output[state].push(cities[i].city);
-
-									output[state].sort();
-								}
-
-								return output;
-							}
-							// else handle csv file
-							const rows = body.replace(/"/g, '').split('\n');
-							let i = 1;
-							l = rows.length;
-
-							for (; l > i++; ) {
-								// expects the first item in the CSV row to be the city name, and the second to be the state id (i.e. FL, IA, CA, etc...)
-								if (rows[i] === '' || rows[i] === undefined)
-									continue;
-								const [city, id] = rows[i].split(',');
-
-								if (id === undefined) continue;
-								if (output[id] === undefined) output[id] = [];
-
-								output[id].push(city);
-							}
-
-							for (let cities of Object.values(output))
-								cities.sort();
-
-							return output;
-						})
-						.then(output => (cache.states = output))
-						.then(() => (cache.active = true))
-						.catch(console.error)
-						.then(() => self.postMessage({ cacheStatus: true }));
+				if (!generator) {
+					cache.states = await fetchIt(data);
+					generator = templateGen();
+					generator.next(data.state_id)
+					postMessage([{ cacheStatus: 'resolved' }, '']);
+					return;
 				}
 
-				if ('state_id' in data === false && data.state_id === undefined)
-					throw 'Missing state_id';
+				function str2ab(str) {
+					var buf = new ArrayBuffer(str.length * 2), // 2 bytes for each char
+						bufView = new Uint16Array(buf),
+						i = 0,
+						l = str.length;
+
+					for (; i < l; i++) bufView[i] = str.charCodeAt(i);
+					return buf;
+				}
+
+				// turn long string into a transferable to transfer close to instantly
+				const tpl = generator.next(data.state_id).value;
+				const buffer = str2ab(tpl);
+				// [ status: {}, data: [] ]
+				return postMessage([{ type: 'TEMPLATE' }, buffer], [buffer]);
+
+				async function fetchIt(data, bodyType = 'text') {
+					cache.type = data.fileType;
+					let output = {};
+					const resp = await fetch(data.file),
+						body = await resp[bodyType]();
+					/*
+					if (cache.type === 'json') {
+						for
+					}
+					*/
+
+					// else handle csv file
+					const rows = body.replace(/"/g, '').split('\n'),
+						l = rows.length;
+
+					let i = 1;
+
+					for (; l > i; i++) {
+						if (rows[i] === '' || rows[i] === undefined) {
+							continue;
+						}
+
+						// expects the first item in the CSV col to be the city name, and the second to be the state id (i.e. FL, IA, CA, etc...)
+
+						const [city, id] = rows[i].split(',');
+
+						if (id === undefined) continue;
+
+						if (output[id] === undefined) {
+							// console.log(id, output);
+							output[id] = [];
+						}
+
+						output[id].push(city);
+					}
+
+					for (let cities of Object.values(output)) {
+						cities.sort();
+					}
+
+					return output;
+				}
+
+				function* templateGen() {
+					let states = cache.states,
+						response;
+					while (true) {
+						let request = yield response;
+
+						if (cache.templates[request] === undefined || cache.templates[request].length === 0)
+							cache.templates[request] = states[request].map(city => `<option value="${city}" data-city-option>${city}</option>`).join('');
+						response = cache.templates[request];
+					}
+				}
+				// console.log('data', data);
+				/*
+				if (data.type === 'START') {
+					cache.type = data.fileType;
+					const resp = await fetch(data.file)
+
+					const output = {};
+					const body =
+						cache.type == 'json'
+							? await resp.json()
+							: await resp.text();
+					if (cache.type === 'json') {
+						for (let [state, cities] of Object.entries(
+							body,
+						)) {
+							if (output[state] === undefined)
+								output[state] = [];
+							let i = 0,
+								l = cities.length;
+
+							for (; l > i++;)
+								output[state].push(cities[i].city);
+
+							output[state].sort();
+						}
+
+						cache.active = true;
+						postMessage({ cacheStatus: true });
+						return;
+					}
+
+					// else handle csv file
+					const rows = body.replace(/"/g, '').split('\n');
+
+					let i = 1;
+					const l = rows.length;
+					// console.log('rows', rows.length);
+					for (; l > i; i++) {
+						if (rows[i] === '' || rows[i] === undefined) {
+							continue;
+						}
+
+						// expects the first item in the CSV col to be the city name, and the second to be the state id (i.e. FL, IA, CA, etc...)
+
+						const [city, id] = rows[i].split(',');
+
+						if (id === undefined) {
+							continue;
+						}
+
+						if (output[id] === undefined) {
+							console.log(id, output);
+							output[id] = [];
+						}
+
+						output[id].push(city);
+					}
+
+					// console.log('endProduct', output);
+					for (let cities of Object.values(output)) {
+						cities.sort();
+					}
+
+					cache.states = output;
+					cache.active = true;
+					postMessage({ cacheStatus: true });
+					return;
+				}
+
+				if ('state_id' in data === false || data.state_id === undefined)
+					return;
 
 				if (
 					cache.templates[data.state_id] !== undefined &&
@@ -93,7 +184,8 @@ export default class DynamicCityDropdown {
 						response: cache.templates[data.state_id],
 					});
 				}
-
+				if (cache.states === undefined) return;
+				console.log('cache', cache);
 				cache.templates[data.state_id] = cache.states[data.state_id]
 					.map(
 						cityName =>
@@ -102,11 +194,29 @@ export default class DynamicCityDropdown {
 					.join('');
 
 				postMessage({ response: cache.templates[data.state_id] });
+				*/
 			});
 		return script;
 	}
 
+	render(buffer, parent) {
+		const str = this.ab2str(buffer);
+		console.log(str);
+		let tpl = document.createElement('template');
+		tpl.innerHTML = str;
+		requestAnimationFrame(() => {
+			while (parent.firstChild) parent.firstChild.remove();
+			parent.append(...tpl.content.children);
+			tpl = null;
+		})
+	}
+
+	ab2str(buf) {
+		return String.fromCharCode.apply(null, new Uint16Array(buf));
+	}
+
 	init() {
+		/*
 		this.form.addEventListener('change', ({ target }) => {
 			if (!target.matches('[data-state]')) return;
 			this.worker.postMessage({
@@ -117,11 +227,9 @@ export default class DynamicCityDropdown {
 				.querySelector('[data-city]');
 			if (this.city.value !== '') this.city.value = '';
 		});
+
 		this.worker.addEventListener('message', ({ data }) => {
-			if (
-				data.cacheStatus === true &&
-				this.form.querySelector('[data-state]').value
-			) {
+			if (this.form.querySelector('[data-state]').value) {
 				this.worker.postMessage({
 					state_id: this.form.querySelector('[data-state]').value,
 				});
@@ -136,5 +244,26 @@ export default class DynamicCityDropdown {
 				if (city) this.city.value = city;
 			});
 		});
+		*/
+		this.worker.postMessage({
+			file: this.file,
+			fileType: this.type,
+		});
+
+		this.worker.onmessage = e => {
+			console.log(e);
+			const [status, buffer] = e.data;
+			if (status.type !== 'TEMPLATE') return;
+			this.render(buffer, this.city);
+		}
+	}
+
+	set states([state, data]) {
+		this.cache.states[state] = data;
+		this.cache.templates[state] = data.map(city => `<option value="${city}" data-city-option>${city}</option>`).join('');
+	}
+
+	get states() {
+		return this.cache.templates;
 	}
 }
